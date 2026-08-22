5a8b8156ea23ac8973aa512062ba6e1d1e5d440d
Eath Mengsrong <athmengsrong2@gmail.com>
2026-08-22 14:54:18 +0700

    Add servers configuration to all SwaggerConfig files

--- operation-service/src/main/java/com/lms/operation/config/SwaggerConfig.java
+++ operation-service/src/main/java/com/lms/operation/config/SwaggerConfig.java
@@ -4,6 +4,9 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -19,4 +22,11 @@ import org.springframework.context.annotation.Configuration;
         bearerFormat = "JWT"
 )
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("https://loan.cavkim.tech").description("Production"))
+                .addServersItem(new Server().url("http://localhost:8080").description("Local Development"));
+    }
 }
